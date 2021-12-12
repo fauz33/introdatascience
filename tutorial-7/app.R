c4e6bc514bad04361492b5f6dcaf8b2ccee20822
fauz33 <fauz33@gmail.com>
2021-12-12 23:15:04 +0800

    Tutorial Week 7

---
+++ tutorial-7/app.R
@@ -0,0 +1,50 @@
+#
+# This is a Shiny web application. You can run the application by clicking
+# the 'Run App' button above.
+#
+# Find out more about building applications with Shiny here:
+#
+#    http://shiny.rstudio.com/
+#
+
+library(shiny)
+library(dplyr)
+library(ggplot2)
+df <- as.data.frame(VADeaths)
+
+# Define UI for application that draws a histogram
+ui <- fluidPage(
+  
+  # Application title
+  titlePanel("Old Faithful Geyser Data"),
+  
+  # Sidebar with a slider input for number of bins 
+  sidebarLayout(
+    sidebarPanel(
+      selectInput("state", "State", choices = names(df)),
+      
+    ),
+    
+    # Show a plot of the generated distribution
+    mainPanel(
+      plotOutput("data")
+    )
+  )
+)
+
+# Define server logic required to draw a histogram
+server <- function(input, output) {
+  
+  data <- reactive(df %>%
+                     select(.data[[input$state]]))
+  
+  output$data <- renderPlot({
+    barplot( df[[input$state]],main = "Death Rates in Virginia",
+             xlab = "Age", ylab = "Rate",ylim = c(0, 100),names.arg=rownames(df))
+  })
+}
+
+# Run the application 
+shinyApp(ui = ui, server = server)
+
+
